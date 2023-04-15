079633aa721929a62ae27eceb236c423422af63a
carolinaboson@gmail.com <carolinaboson@gmail.com>
2023-04-15 16:41:35 -0300

    criação Schema e o Model, utilizar Middlewares necessarios

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -4,12 +4,20 @@ import nc from 'next-connect';
 import{ upload, uploadImagemCosmic } from '../../services/uploadImagemCosmic';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import { PublicacaoModel } from "@/models/PublicacaoModel";
 
 const handler = nc()
     .use(upload.single('file'))
     .post(async (req : any,  res : NextApiResponse<RespostaPadraoMsg>) => {
 
         try{
+            const {userId} = req.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({erro : 'Uruario não encontrado'});
+            }
+
             if(!req || !req.body){
                 return res.status(400).json({erro : 'Parametros de entrada não informados'});
             }
@@ -19,11 +27,20 @@ const handler = nc()
                 return res.status(400).json({erro : 'Descrição não é válida'});
             }
     
-            if(!req.file){
+            if(!req.file || !req.file.originalname){
                 return res.status(400).json({erro : 'Imagem é obrigatória'});
             }
-    
-            return res.status(200).json({msg : 'Publicação está valida'});
+
+            const image = await uploadImagemCosmic(req);
+            const publicacao = {
+                idUsuario : usuario._id,
+                descricao,
+                foto : image.media.url,
+                data : new Date()
+            }
+
+            await PublicacaoModel.create(publicacao);
+            return res.status(200).json({msg : 'Publicação criada com sucesso'});
         }catch(e){
             console.log(e);
             return res.status(400).json({erro : 'Erro ao cadastrar publicação'});
